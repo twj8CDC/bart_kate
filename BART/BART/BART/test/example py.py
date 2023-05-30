dd67b4884e2ea2c5e70ebe68e30e3e8325b2fabc
twj8CDC <twj8@cdc.gov>
2023-05-30 16:03:48 -0400

    moved files to bart_python dir

---
+++ BART/BART/BART/test/example py.py
@@ -0,0 +1,90 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC Swig isn't pre-installed, it can be installed through pip
+
+# COMMAND ----------
+
+# MAGIC
+# MAGIC %sh swig -version
+
+# COMMAND ----------
+
+#pip install swig
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC The example.i and the example.c files are created already in the directory
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Running swig works correctly
+
+# COMMAND ----------
+
+# MAGIC %sh swig -python example.i
+
+# COMMAND ----------
+
+# MAGIC %ls
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC example_wrap.c and the example.py files are now created
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC There are two ways to compile the package.
+# MAGIC First with the python setup.py and the other directly with the C++ compiler
+
+# COMMAND ----------
+
+# MAGIC %sh python setup.py build_ext --inplace
+
+# COMMAND ----------
+
+# MAGIC %sh swig -python example.i
+
+# COMMAND ----------
+
+from sysconfig import get_paths
+from pprint import pprint
+
+info = get_paths()  # a dictionary of key-paths
+
+# pretty print it for now
+pprint(info)
+
+# COMMAND ----------
+
+# MAGIC %sh gcc -v -c example.c example_wrap.c -I/usr/include/python3.10
+
+# COMMAND ----------
+
+from sysconfig import get_paths
+from pprint import pprint
+
+info = get_paths()  # a dictionary of key-paths
+
+# pretty print it for now
+# pprint(info)
+info
+
+# COMMAND ----------
+
+# MAGIC %ls /usr/include/python3.10/
+
+# COMMAND ----------
+
+# MAGIC %sh python --version
+
+# COMMAND ----------
+
+import example
+
+# COMMAND ----------
+
+example.fact(4)
---
+++ BART/BART/BART/test/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""
+setup.py file for SWIG example
+"""
+
+from distutils.core import setup, Extension
+
+
+example_module = Extension('_example',
+                           sources=['example_wrap.c', 'example.c'],
+                           )
+
+setup (name = 'example',
+       version = '0.1',
+       author      = "SWIG Docs",
+       description = """Simple swig example from docs""",
+       ext_modules = [example_module],
+       py_modules = ["example"],
+       )
+
+# setup.py
+
+# from distutils.core import setup, Extension
+
+
+# example_module = Extension('_example', sources=['example.c', 'example.i'])
+
+# setup(name='example', ext_modules=[example_module], py_modules=["example"])
