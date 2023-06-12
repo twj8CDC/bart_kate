d2543da92559345f5fda154d0171237b0f5e633d
ulc0 <ulc0>
2023-06-12 15:20:24 +0000

    test compile

--- BART/BART/BART/inst/cxx-ex/build_sh.py
+++ BART/BART/BART/inst/cxx-ex/build_sh.py
@@ -1,3 +1,10 @@
 # Databricks notebook source
 # MAGIC %sh
+# MAGIC #wget -o rmath.zip https://github.com/statslabs/rmath/zipball/master
+# MAGIC #unzip rmath
+# MAGIC #ls -ll rmath
+
+# COMMAND ----------
+
+# MAGIC %sh
 # MAGIC make wmain.out check
