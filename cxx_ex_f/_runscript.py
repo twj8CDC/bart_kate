a595d7a1cb9d3e5073ff5e0ba1f677809aad1a58
twj8CDC <twj8@cdc.gov>
2023-06-23 14:32:48 -0400

    repo level Rmath and sh runner

--- cxx_ex_f/_runscript.py
+++ cxx_ex_f/_runscript.py
@@ -1,2 +1,15 @@
+#!/usr/bin/env -S LD_LIBRARY_PATH=./slib python options
+
+# import sys
+# print(sys.path)
+# sys.path.append("/home/twj8/bart_kate/cxx_ex_f/slib/")
+
+# import os
+# print(os.environ["LD_LIBRARY_PATH"])
+# os.environ['LD_LIBRARY_PATH'] = os.getcwd()+"/slib/"
+# print(os.environ["LD_LIBRARY_PATH"])
+
+
+
 import pmain
 pmain.pmain()
