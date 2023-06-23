bdf0452e1040e405803ea657f67a536431ca64eb
twj8@cdc.gov <twj8@cdc.gov>
2023-06-23 15:20:30 +0000

    add runner

--- cxx_ex_f/_runner.py
+++ cxx_ex_f/_runner.py
@@ -4,3 +4,39 @@
 # COMMAND ----------
 
 # MAGIC %sh make all
+
+# COMMAND ----------
+
+import pmain
+
+# COMMAND ----------
+
+# MAGIC %sh python3.10 _runscript.py
+
+# COMMAND ----------
+
+# MAGIC %sh echo $LD_LIBRARY_PATH
+
+# COMMAND ----------
+
+# MAGIC %sh LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/Workspace/Repos/twj8@cdc.gov/bart_kate/cxx_ex_f/slib
+
+# COMMAND ----------
+
+import sys
+
+# COMMAND ----------
+
+sys.path.append("/Workspace/Repos/twj8@cdc.gov/bart_kate/cxx_ex_f/slib")
+
+# COMMAND ----------
+
+sys.path
+
+# COMMAND ----------
+
+import pmain
+
+# COMMAND ----------
+
+# MAGIC %sh export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/Workspace/Repos/twj8@cdc.gov/bart_kate/cxx_ex_f/slib
---
+++ cxx_ex_f/_runscript.py
@@ -0,0 +1,2 @@
+import pmain
+pmain.pmain()
